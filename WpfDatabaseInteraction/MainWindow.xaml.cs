9702fce6d649ac82ec43c1e7b7a759f63633da9c
Jacob Morehouse <jacob.morehouse@gmail.com>
2016-12-06 16:55:10 -0800

    Added new record functionality.

--- WpfDatabaseInteraction/MainWindow.xaml.cs
+++ WpfDatabaseInteraction/MainWindow.xaml.cs
@@ -15,16 +15,19 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
+
 public static class dataops {
+
+    const string connstring = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=wpfdatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
     public static void loadDataTable(DataGrid dataGrid1)
     {
-        using (SqlConnection sc = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=wpfdatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"))
+        using (SqlConnection sc = new SqlConnection(connstring))
         {
             sc.Open();
             string sql = "SELECT * FROM users";
             SqlCommand com = new SqlCommand(sql, sc);
-
-                 
+  
             using (SqlDataAdapter adapter = new SqlDataAdapter(com))
             {
                 DataTable dt = new DataTable();
@@ -33,7 +36,31 @@ public static class dataops {
             }
         }
     }
+
+
+    public static int convertint(string intputstr)
+    {
+        int outputid; //this is all just to parse out the int.
+        var temp = Int32.TryParse(intputstr, out outputid);
+        return outputid;
+    }
+
+
+    public static void rundbstring(string inputstring)
+    {
+        SqlConnection sqlConnection1 = new SqlConnection(connstring);
+        SqlCommand cmd = new SqlCommand();
+
+        cmd.CommandText = String.Format(inputstring);
+        cmd.CommandType = CommandType.Text;
+        cmd.Connection = sqlConnection1;
+
+        sqlConnection1.Open();
+        cmd.ExecuteNonQuery();
+        sqlConnection1.Close();
+    }
 }
+
 
 namespace WpfDatabaseInteraction
 {
@@ -46,12 +73,22 @@ namespace WpfDatabaseInteraction
         {
             InitializeComponent();
         }
-
         
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             dataops.loadDataTable(dataGrid);
+        }
+
+        private void buttonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            string addUserString = String.Format("INSERT INTO Users(id, username, password, email) values ({0},'{1}','{2}','{3}')", dataops.convertint(textBoxAddid.Text), textBoxAddName.Text, textBoxAddEmail.Text, textBoxAddPass.Text);
+            dataops.rundbstring(addUserString);
+            dataops.loadDataTable(dataGrid);
+            textBoxAddid.Clear();
+            textBoxAddName.Clear();
+            textBoxAddPass.Clear();
+            textBoxAddEmail.Clear();
         }
     }
 }
